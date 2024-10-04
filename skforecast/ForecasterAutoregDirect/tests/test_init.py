69a3309cbe88a8b1f0ad0abe946eb3d6a99382d2
GinesMeca <ginesmeca11@hotmail.com>
2024-10-04 10:26:16 +0200

    Modifying error message according to new steps-to-list changes

--- skforecast/ForecasterAutoregDirect/tests/test_init.py
+++ skforecast/ForecasterAutoregDirect/tests/test_init.py
@@ -14,8 +14,8 @@ def test_init_TypeError_when_steps_is_not_int():
     """
     steps = 'not_valid_type'
     err_msg = re.escape(
-                f"`steps` argument must be an int greater than or equal to 1. "
-                f"Got {type(steps)}."
+                 "`steps` argument must be an int, 1d numpy ndarray, range, tuple or list. "
+                 f"Got {type(steps)}."
             )
     with pytest.raises(TypeError, match = err_msg):
         ForecasterAutoregDirect(LinearRegression(), lags=2, steps=steps)
